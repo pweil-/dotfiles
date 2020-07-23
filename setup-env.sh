1ac85a66fc7f5a41c1056ab6cce1671e62900842
Paul Weil <pweil@redhat.com>
2020-07-23 15:58:51 -0400

    setup script

---
+++ setup-env.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -eux -o pipefail
+
+# sudo dnf install -y git vim
+
+mkdir ~/{bin,codebase}
+
+###
+# Gimmie
+###
+curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+chmod +x ~/bin/gimme
+
+###
+# profile
+###
+cd ~/codebase
+git clone git@github.com:pweil-/dotfiles.git
+
+ln -s ~/codebase/.profile ~/.profile
+echo "source ~/.profile" >> ~/.bashrc
+
+
+###
+# vim
+###
+ln -s ~/codebase/.vimrc ~/.vimrc
+ln -s ~/codebase/.vim ~/.vim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Be sure to run BundleInstall in vim"
+
+
+###
+# gnome terminal colors
+###
+cd ~/codebase
+git clone https://github.com/aruhier/gnome-terminal-colors-solarized.git
+gnome-terminal-colors-solarized/install.sh
+
+cd ~
